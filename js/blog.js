e73140c483eb49c99d8096c16d9fa9818fba5722
Natalie Chow <cychownat@gmail.com>
2015-12-01 16:38:38 -0800

    Added dropdown menus for filters

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,7 @@
 var blog = {};
 blog.articles = [];
+blog.listAuthor = [];
+blog.listCategory = [];
 
 // import content from blogArticles.js
 blog.importArticles = function() {
@@ -36,6 +38,43 @@ blog.previewArticles = function() {
   });
 };
 
+blog.createFilters = function() {
+  for (var i = 0; i < blog.articles.length; i++) {
+    var temp = blog.articles[i].author;
+    // check if already logged
+    if (blog.listAuthor.indexOf(temp) < 0) {
+      blog.listAuthor.push(temp);
+    }
+  }
+
+  for (var i = 0; i < blog.articles.length; i++) {
+    var temp = blog.articles[i].category;
+    if (blog.listCategory.indexOf(temp) < 0) {
+      blog.listCategory.push(temp);
+    }
+  }
+};
+
+blog.createFilters = function(list, selectId, prop) {
+  for (var i = 0; i < blog.articles.length; i++) {
+    var temp = blog.articles[i][prop];
+    if (list.indexOf(temp) < 0) {
+      list.push(temp);
+    }
+  }
+  for (var i = 0; i < list.length; i++) {
+    var $newOption = $(selectId).children(':first-child').clone();
+    $newOption.attr('value', prop.substring(0, 3) + i);
+    $newOption.html(list[i]);
+    $(selectId).append($newOption);
+  };
+};
+
+blog.showFilters = function() {
+  blog.createFilters(blog.listAuthor, '#filter-by-author', 'author');
+  blog.createFilters(blog.listCategory, '#filter-by-category', 'category');
+};
+
 $(function() {
   var today = new Date();
   // import & sort through raw data
@@ -47,4 +86,6 @@ $(function() {
 
   // truncate posts to the first paragraph
   blog.previewArticles();
+
+  blog.showFilters();
 });
