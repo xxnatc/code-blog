5fd483bae0cfd59b009cafc8bae53a314938260a
suhk <kennethgenesuh@gmail.com>
2015-12-04 14:33:33 -0800

    Added live preview to secret.js

---
+++ js/secret.js
@@ -0,0 +1,17 @@
+$('#preview-button').on('click', function(event) {
+  event.preventDefault();
+
+  var prop = {};
+  prop.title = $('#article-title').val();
+  prop.author = $('#article-author').val();
+  prop.authorUrl = $('#article-author-url').val();
+  prop.category = $('#article-category').val();
+  prop.publishedOn = new Date();
+
+  $articleBody = $('#article-body').val();
+  prop.body = marked($articleBody);
+
+  var newArticle = new Article(prop);
+  $('#home').children().remove();
+  newArticle.toHTML();
+});
