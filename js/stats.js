b1c244694a548216bd0f96e3a9cc1b6c0eacd5f8
Natalie Chow <cychownat@gmail.com>
2015-12-08 15:54:54 -0800

    Cleaned up code

--- js/stats.js
+++ js/stats.js
@@ -3,15 +3,15 @@ var stats = {};
 stats.displayStats = function() {
   stats.showTotalArticles();
   stats.showTotalAuthors();
-
+  // execute word count
   stats.countAll();
+  
   stats.showTotalWords();
-
-
   stats.showAvgOverall();
   stats.showAvgPerAuthor();
 };
 
+/* ==================== write on page ==================== */
 stats.showTotalArticles = function() {
   var $headline = $('<h3>Number of Articles</h3>');
   var $number = $('<h2>' + blog.articles.length + '</h2>');
@@ -24,8 +24,26 @@ stats.showTotalAuthors = function() {
   $('#num-authors').append([$headline, $number]);
 };
 
+stats.showTotalWords = function() {
+  var $headline = $('<h3>Number of Words</h3>');
+  var $number = $('<h2>' + stats.listWordCount.reduce(stats.sum) + '</h2>');
+  $('#num-words').append([$headline, $number]);
+};
 
+stats.showAvgOverall = function() {
+  var $headline = $('<h3>Average length of article</h3>');
+  var $number = $('<h2>' + stats.listWordCount.reduce(stats.sum) / blog.articles.length + ' words</h2>');
+  $('#avg-overall').append([$headline, $number]);
+};
 
+stats.showAvgPerAuthor = function() {
+  var $headline = $('<h3>Average article length per author</h3>');
+  $('#avg-authors').append($headline);
+  stats.avgWordsPerAuthor();
+};
+
+/* ==================== word count ==================== */
+// count number of words on each article
 stats.listWordCount = [];
 stats.countAll = function() {
   blog.articles.forEach(function(article) {
@@ -37,51 +55,29 @@ stats.countArticle = function(article) {
   return article.body.split(' ').length;
 };
 
-stats.showTotalWords = function() {
-  var $headline = $('<h3>Number of Words</h3>');
-  var $number = $('<h2>' + stats.listWordCount.reduce(stats.sum) + '</h2>');
-  $('#num-words').append([$headline, $number]);
-};
+/* ==================== word count per author ==================== */
 
-
-stats.showAvgOverall = function() {
-  var $headline = $('<h3>Average length of article</h3>');
-  var $number = $('<h2>' + stats.listWordCount.reduce(stats.sum) / blog.articles.length + ' words</h2>');
-  $('#avg-overall').append([$headline, $number]);
-};
-
-
-stats.showAvgPerAuthor = function() {
-  var $headline = $('<h3>Average article length per author</h3>');
-  $('#avg-authors').append($headline);
-  stats.avgWordsPerAuthor();
-};
-
-stats.appendAvgPerAuthor = function(author, count) {
-  var comp = $('<p>');
-  comp.text(author + ': ' + count);
-  $('#avg-authors').append(comp);
-};
-
-stats.wpa = [];
 stats.avgWordsPerAuthor = function() {
   blog.listAuthor.forEach(stats.wordsPerAuthor);
 };
 
 stats.wordsPerAuthor = function(element, index, array) {
-// list of article indices
   var wpa = blog.listAuthorIndex[index].reduce(function(acc, current) {
     return acc + stats.listWordCount[current];
   }, 0);
-  console.log(blog.listAuthor[index], wpa);
-  stats.appendAvgPerAuthor(blog.listAuthor[index], wpa);
-  stats.wpa.push(wpa);
+  stats.appendPerAuthor(blog.listAuthor[index], wpa);
 };
 
+stats.appendPerAuthor = function(author, count) {
+  var comp = $('<p>');
+  comp.text(author + ': ' + count);
+  $('#avg-authors').append(comp);
+};
+
+/* ==================== helpers ==================== */
 stats.sum = function(acc, num) {
   return acc + num;
 };
-
 
 
 $(function() {
