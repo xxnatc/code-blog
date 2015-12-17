04707f562a38ac375bcd532fa285902e0f504eef
Natalie Chow <cychownat@gmail.com>
2015-12-16 22:36:51 -0800

    Filter by author and category now works and populates correctly

--- js/articleController.js
+++ js/articleController.js
@@ -4,6 +4,7 @@ articleController.index = function() {
   Article.loadAll(function() {
     articleView.index();
   });
+  articleController.handleFilters();
 };
 
 articleController.selectId = function(ctx) {
@@ -19,5 +20,34 @@ articleController.selectAuthor = function(ctx) {
   console.log(author);
   Article.findByAuthor(author, function(data) {
     articleView.selection(data);
+  });
+};
+
+articleController.handleFilters = function() {
+  $('#filter-by-author').on('change', function(event) {
+    event.preventDefault();
+    if ($(this).find('option:selected').attr('value') === 'reset') {
+      $('#articles').fadeIn();
+    } else {
+      var author = $(this).find('option:selected').text();
+      Article.findByAuthor(author, function(data) {
+        articleView.selection(data);
+      });
+    }
+    // reset the other dropdown to prevent multiple selections
+    $('#filter-by-category').find('option[value=reset]').attr('selected', true);
+  });
+
+  $('#filter-by-category').on('change', function(event) {
+    event.preventDefault();
+    if ($(this).find('option:selected').attr('value') === 'reset') {
+      $('#articles').fadeIn();
+    } else {
+      var category = $(this).find('option:selected').text();
+      Article.findByCategory(category, function(data) {
+        articleView.selection(data);
+      });
+    }
+    $('#filter-by-author').find('option[value=reset]').attr('selected', true);
   });
 };
